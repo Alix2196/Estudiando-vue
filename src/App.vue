38f174d76a41dd8b2642515c0278334aaa409e33
Alix Meneses <angelica962008@hotmail.com>
2022-10-27 11:52:54 -0500

    Inicio proyecto vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<!-- Se coloca la logica -->
+<script setup>
+
+</script>
+
+
+<!-- Estructura -->
+<template>
+    <h1>Hola vue js</h1>
+</template>
+
+<!-- Se coloca el estilo -->
+<style>
+
+  h1 {
+    color :violet;
+  }
+
+</style>
